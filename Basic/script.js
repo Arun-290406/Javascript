b7aff2b578f1e4cc5e2c7002d35daa100ed71b3e
Arun-290406 <727823tuit013@skct.edu.in>
2025-08-29 11:07:06 +0530

    new Basic file created

---
+++ Basic/script.js
@@ -0,0 +1,17 @@
+alert("Welcome to the Html code");
+const x=100;
+console.log(x);
+console.error("Error");
+console.warn("warning");
+
+console.group("Simple");
+console.debug("debugged");
+console.error("Error");
+console.warn("warning");
+console.groupEnd();
+
+console.table({name:'Arun',Age:18});
+
+const style="padding: 10px;background-color: black;color: red";
+console.log("%cHello Everyone",style);
+
