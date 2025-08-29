2c096970693174fc10399af490e955d80727ca00
Arun-290406 <727823tuit013@skct.edu.in>
2025-08-29 22:53:13 +0530

    29-8-25 update

---
+++ 03-Datatype/script.js
@@ -0,0 +1,33 @@
+//string 
+
+const name="arun";
+const output=name;
+console.log(output, typeof output);
+
+//number
+const age=19;
+const anum1=202.78;
+console.log(age, typeof age);
+console.log(anum1, typeof anum1);
+
+//boolean
+const bool=true;
+console.log(bool,typeof bool);
+
+//null
+const value=null;
+console.log(value,typeof null);
+
+//undefined
+let val;
+console.log(val,typeof val);
+
+//BigInt
+const big=1345123445n;
+console.log(big,typeof big);   
+
+//Reference Type
+``
+const arr=[1,2,3,45,6]
+arr.push(33,55);
+console.log(arr, typeof arr);
---
+++ 04-stack-heap/script1.js
@@ -0,0 +1,16 @@
+//Values are stored in a stack
+
+const name='arun';
+const age=18;
+console.log(name, age);
+
+//REFERENCE VARIABLE stored in the heap
+
+const person={
+    name:'kaithi',
+    age:21,
+};
+let lastperson=person;
+lastperson.name='vetrivel';
+console.log(lastperson);
+
